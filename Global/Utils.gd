648906f3d93de1d43e8b8c457f44a7721756870d
simon <simon.c.yacoub@gmail.com>
2024-02-12 23:15:38 -0500

    Fixed the Highscore feature Added a Record Book feature so players can save and keep track of high scores Added a main menu button to death screen
    Signed-off-by: simon <simon.c.yacoub@gmail.com>

--- Global/Utils.gd
+++ Global/Utils.gd
@@ -2,6 +2,7 @@ extends Node
 
 const SAVE_PATH = "res://savegame.bin"
 const HIGHSCORE_PATH = "res://highscore.bin"
+var record_book = {}
 
 func saveGame():
 	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
@@ -22,62 +23,43 @@ func loadGame():
 				Game.PlayerHp = current_line["PlayerHP"]
 				Game.Gold = current_line["Gold"]
 	file.close()
+	loadRecordBook()
 
 func saveTime():	
-	var file = FileAccess.open(HIGHSCORE_PATH, FileAccess.READ_WRITE)
-	if FileAccess.file_exists(HIGHSCORE_PATH):
-		if not file.eof_reached():
-			var current_line = ""
-			# Jump to line with level and score for that level 
-			for x in Game.Level:
-				current_line = JSON.parse_string(file.get_line())
-			if !current_line: #If empty overwrite line
-				var data: Dictionary = {str(Game.Level) : Game.TimeElapsed}
-				var jstr = JSON.stringify(data)
-				file.store_line(jstr)
-			elif current_line.has(str(Game.Level)): # if there is a score compare it and then overwrite it or do nothing
-				var previousTimeForThisLevel = current_line[str(Game.Level)]
-				if previousTimeForThisLevel:
-					if Game.TimeElapsed < previousTimeForThisLevel:
-						var data: Dictionary = {str(Game.Level) : Game.TimeElapsed}
-						var jstr = JSON.stringify(data)
-						file.seek(0)
-						for x in Game.Level - 1:
-							current_line = JSON.parse_string(file.get_line())									
-						file.store_line(jstr)
-			else:
-				var data: Dictionary = {str(Game.Level) : Game.TimeElapsed}
-				var jstr = JSON.stringify(data)
-				file.seek(0)
-				for x in Game.Level - 1:
-					current_line = JSON.parse_string(file.get_line())									
-					file.store_line(jstr)
-			file.close()
-	else:
-		file = FileAccess.open(HIGHSCORE_PATH, FileAccess.WRITE)
-		var data: Dictionary = {str(Game.Level) : Game.TimeElapsed}
-		var jstr = JSON.stringify(data)
-		file.store_line(jstr)	
-		file.close()
-						
-func getHighScore():
+	if record_book.has(str(Game.Level)):
+		var previousTimeForThisLevel = record_book.get(str(Game.Level))
+		if Game.TimeElapsed < previousTimeForThisLevel:
+			record_book[str(Game.Level)] = Game.TimeElapsed			
+			saveRecordBook()
+		
+func saveRecordBook():
+	var file = FileAccess.open(HIGHSCORE_PATH, FileAccess.WRITE)
+	var jstr = JSON.stringify(record_book)
+	file.store_string(jstr)
+	return
+	
+func loadRecordBook():
 	var file = FileAccess.open(HIGHSCORE_PATH, FileAccess.READ)
 	if FileAccess.file_exists(HIGHSCORE_PATH):
-		if not file.eof_reached():
-			var current_line = JSON.parse_string(file.get_line())
-			for x in Game.Level - 1:
-				current_line = JSON.parse_string(file.get_line())
-			if current_line:
-				return current_line[str(Game.Level)]			
-	file.close()	
+		var json = JSON.new()
+		var json_string = file.get_as_text()
+		var error = json.parse(json_string)
+		if error == OK:
+			var data_received = json.data
+			if typeof(data_received) == TYPE_DICTIONARY:
+				record_book = data_received
+						
+func getHighScore():
+	if record_book.has(str(Game.Level)):
+		return record_book.get(str(Game.Level))
 	
 func getRecords():
 	var records = ""
-	var file = FileAccess.open(HIGHSCORE_PATH, FileAccess.READ)
-	if FileAccess.file_exists(HIGHSCORE_PATH):
-		while not file.eof_reached():
-			records += file.get_line() + "\n"		
+	var keys = record_book.keys()
+	var values = record_book.values()
+	for x in 10:
+		if record_book.has(str(x)):
+			records += "Level " + str(x) + ": " + str(record_book.get(str(x))) + "\n"
 	return records
-	file.close()	
 		
 
